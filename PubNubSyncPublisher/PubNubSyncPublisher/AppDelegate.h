a172c31f9d35ffe7226d968b8a6c2ab051e696c3
Jordan Zucker <jordan.zucker@gmail.com>
2015-10-01 01:04:25 -0700

    Basic project, need to implement more effectively

---
+++ PubNubSyncPublisher/PubNubSyncPublisher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PubNubSyncPublisher
+//
+//  Created by Jordan Zucker on 10/1/15.
+//  Copyright © 2015 pubnub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PubNubSyncPublisher/PubNubSyncPublisherTests/PubNubSyncPublisherTests.m
@@ -0,0 +1,39 @@
+//
+//  PubNubSyncPublisherTests.m
+//  PubNubSyncPublisherTests
+//
+//  Created by Jordan Zucker on 10/1/15.
+//  Copyright © 2015 pubnub. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PubNubSyncPublisherTests : XCTestCase
+
+@end
+
+@implementation PubNubSyncPublisherTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
